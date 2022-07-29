9179c2536a6290ceb31381a385875d587c4a4a45
vedant prasad priya karle <59387428+vedantkarle@users.noreply.github.com>
2022-07-29 13:27:10 +0530

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { NumbersCollection } from './NumbersCollection';
+import { Sorter } from './Sorter';
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);
