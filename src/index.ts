3fd0fbf288ab79b5f6e9a678d548eca88b6a7654
vedant prasad priya karle <59387428+vedantkarle@users.noreply.github.com>
2022-07-29 17:34:53 +0530

    added characters collection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { CharactersCollection } from './CharactersCollection';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+const charactersCollection = new CharactersCollection("edbca");
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
